defeb1881bbf0b2549a7864704ec08f2f2e8af9c
sggdv <494168224@qq.com>
2015-09-02 00:59:56 +0800

    define

--- fahr.c
+++ fahr.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 main () 
 {
 	float fahr, celsius;
-	int lower, upper, step;
 
-	lower = 0;
-	upper = 50;
-	step = 4;
-
-	fahr = lower;
+	fahr = LOWER;
 
 	printf("%4s%15s\n", "fahr", "celsius");
 
-	while (fahr <= upper) {
+	while (fahr <= UPPER) {
 		celsius = 5.0 / 9 * (fahr - 32);
 		printf("%4.0f%15.1f\n", fahr, celsius);
-		fahr = fahr + step;
+		fahr = fahr + STEP;
 	}
 }
