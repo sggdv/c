60dbfba9cc561046c74ca2cef87cb5978f80191a
Kim <331599158@qq.com>
2015-09-09 16:07:01 +0800

    add comments

--- helloworld.c
+++ helloworld.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
+/*
+	C语言入门，第1个C语言程序，打印 Hello World 字符。
+
+	在C语言中，main()函数是程序执行的入口函数。printf 是stdio.h中提供的函数，因此需要引入
+	stdio.h 头文件。
+
+	注意：
+
+	#include <stdio.h>   
+	#include 'stdio.h'
+
+	以上2种include的不同之处在于，使用尖括号的<stdio.h>时，程序会先从系统目录中找stdio.h;
+	而使用单引号的'stdio.h'时，程序是从当前目录中找stdio.h。本例中，因此 stdio.h 这个文件
+	是在系统目录内，因些使用 <stdio.h> 要比 'stdio.h' 的执行效率要快。
+
+	结论: 对于系统自带的头文件，使用 #include <*.h> 方式引入；其他的使用 #include '*.h' 
+	方式引入。
+*/
 main()
 {
 	printf("Hello World\n");
--- power.c
+++ power.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 
-int power(int m, int n);
+/*
+	函数原型。
+	使用函数原型是ANSI C的一个重要特点。它的作用主要是利用它在程序的编译阶段对调用函数的合法
+	性进行全面检查。
+*/
+int power(int base, int n);
 
 main()
 {
 	int i;
 
 	for (i = 0; i < 10; ++i)
-		printf("%d %d %d\n", i, power(2, i), power(-3, i) );
+		printf("%d %d %d\n", i, power(2, i), power(-3, i));
 
 	return 0;
 }
 
+/*
+	函数的作用是，求底数base的n次冥。
+
+	在C语言的函数定义中，参数列表中的参数为局部参数，其作用范围只限函数内部。
+*/
 int power(int base, int n)
 {
 	int i, p;
