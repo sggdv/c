d9958b39467673bd38ad56e1b405528fa142ae11
Kim <494168224@qq.com>
2016-01-06 12:30:41 +0800

    研究char类型的取值范围。

---
+++ charlen.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/*
+ * 测试char类型的取值范围。
+ */
+int main() 
+{
+	unsigned char c = 0xff;
+	printf("c=%d\n", ++c);
+}
--- power.c
+++ power.c
@@ -7,19 +7,11 @@ int power(int base, int n);
 
 int power(int base, int n)
 {
-	int i, p;
-	p = 1;
+	int i;
+	int p = 1;
+
 	for (i = 1; i <= n; ++i)
 		p = p * base;
+
 	return p;
-}
-
-int main()
-{
-	int i;
-
-	for (i = 0; i < 10; ++i)
-		printf("%d %d %d\n", i, power(2, i), power(-3, i));
-
-	return 0;
 }
