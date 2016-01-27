00864a3fc4167aa8aece5d2cd915ebcc79cab8ac
Kim <494168224@qq.com>
2016-01-27 18:00:12 +0800

    调整注释。明确helloworld.c的目标。

--- helloworld.c
+++ helloworld.c
@@ -1,32 +1,23 @@
+/*
+ * C语言入门，第1个C语言程序，打印 Hello World 字符。
+ *
+ * 在C语言中，main()函数是程序执行的入口函数。也就是执行C语言程序时，系统会首先
+ * main()函数开始。
+ *
+ * 打印Hello World字符串，需要借助C语言标准库stdio.h中提供的printf函数，因此需要
+ * 引入 stdio.h 头文件。
+ *
+ * 写完代码之后，通过以下命令编译源文件
+ * 	cc helloworld.c 
+ * 该命令会生成一个可以执行的a.out文件。执行
+ * 	./a.out
+ * 程序会在控制台输出"Hello World"字样。
+ *
+ * 至此，第1个C语言程序就完成了。
+ */
 #include <stdio.h>
 
-/*
-	C语言入门，第1个C语言程序，打印 Hello World 字符。
-
-	在C语言中，main()函数是程序执行的入口函数。printf 是stdio.h中提供的函数，因此需要引入
-	stdio.h 头文件。
-
-	注意：
-
-	#include <stdio.h>   
-	#include "stdio.h"
-
-	以上2种include的不同之处在于，使用尖括号的<stdio.h>时，程序会先从系统目录中找stdio.h;
-	而使用双引号的"stdio.h"时，程序是从当前目录中找stdio.h。本例中，由于 stdio.h 这个文件
-	是在系统目录内，因此使用 <stdio.h> 要比 "stdio.h" 的执行效率要快。
-
-	结论: 对于系统自带的头文件，使用 #include <*.h> 方式引入；其他的使用 #include "*.h" 
-	方式引入。
-*/
-/*
- * 一个C语言程序，不论其大小如何，都是由函数和变量组成的。
- * 函数中包含一些语句，以指定所要执行的计算操作；
- * 变量，则用于存储计算过程中使用的值；
- *
- * 关于代码风格：
- * 由于制表符为8个字符，所以缩进也为8个字符。
- */
 main()
 {
-	printf("Hello World\n");
+	printf("Hello World!\n");
 }
